9e18bddacd94d742c2846d9c5252336deb7ae6f7
prabal kuinkel <prabalkuinkel10@gmail.com>
2024-04-05 21:21:46 +0700

    database problem

--- database/migrations/2024_04_03_140546_create_serviceproviders_table.php
+++ database/migrations/2024_04_03_140546_create_serviceproviders_table.php
@@ -20,13 +20,14 @@ return new class extends Migration
     $table->char('address',30);
     $table->integer('exp_year');
     $table->char('category',30);
+    $table->integer('price');
+    $table->char('status',20);
 
-
-
-    $table->foreignId('c_id')->constrained(
+    $table->foreignId('c_id')->constrained
+    (
         table: 'categories', indexName: 'id'
     );
-        });
+    });
     }
 
     /**
@@ -36,4 +37,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('serviceproviders');
     }
+    
 };
