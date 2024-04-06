cb432bb2f6f52678ae7c986276a64c00fd5d2dae
anilghale123 <ghalepasang5@gmail.com>
2024-04-06 17:35:38 +0545

    updated UI portion of home

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -11,17 +11,17 @@
 <body class="bg-gray-100">
     <main class="container mx-auto p-4">
         <!-- Hero Section -->
-        <section class="bg-white p-3">
+        <section class="  ">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div class="bg-pink-100">
-                    <h1 class="    font-bold text-xlg md:max-w-md">Subidha Ghar</h1>
-                    <h2 class=" font-bold text-6xl md:max-w-md text-center md:text-left md:text-2xl lg:text-2xl ">
-    At your door to solve all your housing problems...
-</h2>
+                <div class=" flex flex-col items-center p-5">
+                    <!-- <h1 class="font-bold text-3xl   md:max-w-md p-5">Subidha Ghar</h1> -->
+                    <h1 class="font-bold  md:max-w-md  md:text-left md:text-2xl lg:text-6xl ">At your door to solve all your
+                    <span class="text-green-500">housing </span>
+  <span class="text-red-500">problems...</span>                    </h1>
                 </div>
-                <div class="bg-pink-200">
-                   <img src="/service.png" alt="Service Image" class="max-w-62 max-h-51  rounded-xl">
-                    
+                <div class="">
+                    <img src="/service.png" alt="Service Image" class="max-w-62 max-h-51  rounded-xl">
+
                 </div>
             </div>
         </section>
@@ -31,122 +31,36 @@
         <!-- Service Section -->
         <section class="my-8">
             <h1 class="text-2xl font-semibold">Services Section</h1>
-           
+
             <div class="my-4">
-            <label for="category">Select a category:</label>
-    <select id="category" class="w-30 h-10 px-4 py-2 border rounded-md">
-    <option value="plumber">Plumber</option>
-    <option value="carpenter">Carpenter</option>
-    <option value="tutor">Tutor</option>
-    <option value="electrician">Electrician</option>
-    </select>
-          
-            <button class="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Filter</button>
+                <label for="category">Select a category:</label>
+                <select id="category" class="w-30 h-10 px-4 py-2 border rounded-md">
+                    <option value="plumber">Plumber</option>
+                    <option value="carpenter">Carpenter</option>
+                    <option value="tutor">Tutor</option>
+                    <option value="electrician">Electrician</option>
+                </select>
+
+                <button class="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Filter</button>
             </div>
 
             <!-- end of search n filter button -->
-           
+
             <!-- card wrapper starts here -->
             <div class="grid grid-cols-1 md:grid-cols-4 auto-cols-fr gap-5">
-                
-            <!-- electrisian card -->
-            @foreach($categories as $category)
+
+                <!-- electrisian card -->
+                @foreach($categories as $category)
                 <div class="card bg-white rounded-lg p-4 mt-4">
                     <h2 class="text-xl font-semibold">{{$category['name']}}</h2>
                     <img src="/electrisian.jpg" alt="electrisian">
                     <p class="text-gray-700 mt-2">{{$category['description']}}</p>
-                
-                    <button onclick="window.location.href='/sewa/{{ $category->id }}'" 
-            class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
-        Explore
-    </button>                </div>
+
+                    <button onclick="window.location.href='/sewa/{{ $category->id }}'" class=" mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
+                        Explore
+                    </button>
+                </div>
                 @endforeach
-                <!-- end of electrisian card -->
-
-
-                <!-- Plumber Card -->
-                <!-- <div class="card bg-white rounded-lg p-4 mt-4">
-                    <h2 class="text-xl font-semibold">Plumber</h2>
-                    <img src="/plumbing.jpg" alt="electrisian">
-
-                    <p class="text-gray-700 mt-2">Details about the plumbing service:</p>
-                    <ul class="list-disc ml-6 mt-2">
-                        <li>abc</li>
-                        <li>xyz</li>
-                        <li>hahaha</li>
-                    </ul>
-                    <button onclick="window.location.href='/plumber'" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Explore</button>
-                </div> -->
-                <!-- end of plumber card -->
-
-                <!-- Carpenter Card -->
-                <!-- <div class="card bg-white rounded-lg p-4 mt-4">
-                    <h2 class="text-xl font-semibold">Carpenter</h2>
-                    <img src="/carpenter.jpg" alt="electrisian">
-
-                    <p class="text-gray-700 mt-2">Details about the plumbing service:</p>
-                    <ul class="list-disc ml-6 mt-2">
-                        <li>abc</li>
-                        <li>xyz</li>
-                        <li>hahaha</li>
-                    </ul>
-                    <button onclick="window.location.href='/carpenter'" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Explore</button>
-                </div> -->
-                <!-- end of carpenter card -->
-
-                <!-- Tutor Card -->
-                <!-- <div class="card bg-white rounded-lg p-4 mt-4">
-                <h2 class="text-xl font-semibold">Tutor</h2>
-
-                <img src="/tutor.jpg" alt="tutor">
-
-                    <p class="text-gray-700 mt-2">Details about the tutoring service:</p>
-                    <ul class="list-disc ml-6 mt-2">
-                        <li>abc</li>
-                        <li>xyz</li>
-                        <li>hahaha</li>
-                    </ul>
-                    <button onclick="window.location.href='/tutor'" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Explore</button>
-                </div> -->
-                <!-- end of tutor card -->
-
-                <!-- Plumber Card -->
-                <!-- <div class="card bg-white rounded-lg p-4 mt-4">
-                    <h2 class="text-xl font-semibold">Plumber</h2>
-                    <p class="text-gray-700 mt-2">Details about the plumbing service:</p>
-                    <ul class="list-disc ml-6 mt-2">
-                        <li>abc</li>
-                        <li>xyz</li>
-                        <li>hahaha</li>
-                    </ul>
-                    <button onclick="window.location.href='/plumber'" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Explore</button>
-                </div> -->
-
-                <!-- Carpenter Card -->
-                <!-- <div class="card bg-white rounded-lg p-4 mt-4">
-                    <h2 class="text-xl font-semibold">Carpenter</h2>
-                    <p class="text-gray-700 mt-2">Details about the plumbing service:</p>
-                    <ul class="list-disc ml-6 mt-2">
-                        <li>abc</li>
-                        <li>xyz</li>
-                        <li>hahaha</li>
-                    </ul>
-                    <button onclick="window.location.href='/carpenter'" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Explore</button>
-                </div> -->
-
-                <!-- Tutor Card -->
-                <!-- <div class="card bg-white rounded-lg p-4 mt-4">
-                    <h2 class="text-xl font-semibold">Tutor</h2>
-                    <p class="text-gray-700 mt-2">Details about the tutoring service:</p>
-                    <ul class="list-disc ml-6 mt-2">
-                        <li>abc</li>
-                        <li>xyz</li>
-                        <li>hahaha</li>
-                    </ul>
-                    <button onclick="window.location.href='/tutor'" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Explore</button>
-                </div> -->
-
-
 
             </div>
         </section>
@@ -154,40 +68,40 @@
 
         <!-- review section starts here -->
         <section title="review-section">
-        <h1 class="text-xl font-semibold">What our User Says</h1>   
-          
-        <div class="grid grid-cols-1 md:grid-cols-4 auto-cols-fr gap-5">
+            <h1 class="text-xl font-semibold">What our User Says</h1>
 
-             <div class="card bg-white rounded-lg p-4 mt-4">
+            <div class="grid grid-cols-1 md:grid-cols-4 auto-cols-fr gap-5">
+
+                <div class="card bg-white rounded-lg p-4 mt-4">
                     <h2 class="text-xl font-semibold">Prabal Kuinkel</h2>
                     <p class="text-gray-700 mt-2">Prabal Ko Experience Yeta</p>
-                     <p>⭐⭐⭐⭐⭐</p>
+                    <p>⭐⭐⭐⭐⭐</p>
                 </div>
 
                 <div class="card bg-white rounded-lg p-4 mt-4">
                     <h2 class="text-xl font-semibold">Sova Kushwaha</h2>
                     <p class="text-gray-700 mt-2">Sova Ko Experience Yeta</p>
-                     <p>⭐⭐⭐⭐⭐</p>
+                    <p>⭐⭐⭐⭐⭐</p>
                 </div>
 
                 <div class="card bg-white rounded-lg p-4 mt-4">
                     <h2 class="text-xl font-semibold">Anil Ghale</h2>
                     <p class="text-gray-700 mt-2">Anil Ko Experience Yeta</p>
-                     <p>⭐⭐⭐⭐⭐</p>
+                    <p>⭐⭐⭐⭐⭐</p>
                 </div>
 
                 <div class="card bg-white rounded-lg p-4 mt-4">
                     <h2 class="text-xl font-semibold">Swornim Thapa</h2>
                     <p class="text-gray-700 mt-2">Swornim Ko Experience Yeta</p>
-                     <p>⭐⭐⭐⭐⭐</p>
+                    <p>⭐⭐⭐⭐⭐</p>
                 </div>
 
 
-        </div>
+            </div>
 
         </section>
 
-       
+
 
     </main>
 </body>
